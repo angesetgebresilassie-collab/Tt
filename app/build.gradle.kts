aa749610776bbad6338260bf59e0b7964a5fd9b7
angesetgebresilassie-collab <angesetgebresilassie@gmail.com>
2026-09-18 21:22:07 +0300

    Improve freeform launching and bump app version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,29 +2,22 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 android {
     namespace = "com.angesetgebresilassie.tt"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.angesetgebresilassie.tt"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
-
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
