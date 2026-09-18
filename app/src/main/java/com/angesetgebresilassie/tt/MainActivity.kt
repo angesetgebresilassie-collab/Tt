d9ab7f2df02f434b15aad176b295d5f2068f327d
angesetgebresilassie-collab <angesetgebresilassie@gmail.com>
2026-09-18 21:32:32 +0300

    Fix missing PackageManager import in launcher status

--- app/src/main/java/com/angesetgebresilassie/tt/MainActivity.kt
+++ app/src/main/java/com/angesetgebresilassie/tt/MainActivity.kt
@@ -2,6 +2,7 @@ package com.angesetgebresilassie.tt
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
