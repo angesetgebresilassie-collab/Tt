421770cf18a861838fd7f5d016610df73a994d40
angesetgebresilassie-collab <angesetgebresilassie@gmail.com>
2026-09-18 22:47:54 +0300

    Bump app version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.angesetgebresilassie.tt"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
